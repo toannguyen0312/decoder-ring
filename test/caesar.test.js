ee8117c62b8a3e9487d867cbbe846918c1b9c8df
Toan Nguyen <80366569+toannguyen0312@users.noreply.github.com>
2022-09-13 16:25:38 -0700

    Update caesar.test.js

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,31 @@
 // Write your tests here!
+const caesar = require('../src/caesar.js').caesar;
+
+const expect = require("chai").expect
+
+describe("caesar", () => {
+  it("should return false if shift is greater than 25", () => {
+    const result = caesar('', 26);
+    expect(result).to.equal(false);
+  });
+  it("should return false if shift is less than -25", () => {
+    const result = caesar('', -26);
+    expect(result).to.equal(false);
+  });
+  it("should return false if shift is equal to 0", () => {
+    const result = caesar('', 0);
+    expect(result).to.equal(false);
+  });
+  it("should encode thinkful message with shift 3", () => {
+    const result = caesar('thinkful', 3);
+    expect(result).to.equal('wklqnixo');
+  });
+   it("should encode thinkful message with shift -3", () => {
+    const result = caesar('thinkful', -3);
+    expect(result).to.equal('qefkhcri');
+  });
+   it("should decode wklqnixo message with shift 3", () => {
+    const result = caesar('wklqnixo', 3, false);
+    expect(result).to.equal('thinkful');
+  });
+})
