492761786d76f1f32ba2113bf3d5aca13b06a00d
Toan Nguyen <80366569+toannguyen0312@users.noreply.github.com>
2022-09-13 16:26:22 -0700

    Update substitution.test.js

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,15 @@
 // Write your tests here!
+const substitution = require('../src/substitution.js').substitution;
+
+const expect = require("chai").expect
+
+describe("subtitution", () => {
+  it("should return decode message", () => {
+    const result = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");;
+    expect(result).to.equal('jrufscpw');
+  });
+  it("should return encode message", () => {
+    const result = substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false);
+    expect(result).to.equal('thinkful');
+  });
+});
