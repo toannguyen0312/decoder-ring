b65fa7c4a73709359118d4c5441c3e9b80eced58
Toan Nguyen <80366569+toannguyen0312@users.noreply.github.com>
2022-09-13 16:26:00 -0700

    Update polybius.test.js

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,15 @@
 // Write your tests here!
+const polybius = require('../src/polybius.js').polybius;
+
+const expect = require("chai").expect
+
+describe("polybius", () => {
+  it("should return decode message", () => {
+    const result = polybius('thinkful', true);
+    expect(result).to.equal('4432423352125413');
+  });
+  it("should return encode message", () => {
+    const result = polybius('3251131343 2543241341', false);
+    expect(result).to.equal('hello world');
+  });
+});
